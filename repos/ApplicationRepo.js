0ed297abaa4f9a95d8573b4f1e721599ebf2cbdd
Denis Pesherin <d.pesherin@it-projects.pro>
2025-12-23 01:48:25 +0300

    fix styles

--- repos/ApplicationRepo.js
+++ repos/ApplicationRepo.js
@@ -31,4 +31,38 @@ export class ApplicationRepo
             return false
         }
     }
+
+    async getList(select = [], filter = [], limit = null, offset = null){
+        let selectVal = ""    
+        if(select.length > 0){
+                selectVal = "*"
+            }else{
+                selectVal = select.join(", ")
+            }
+            let sqlStatement = `SELECT ${selectVal} FROM applications`
+            let filterIndex = 1
+            let vals = []
+            let filterArr = []
+            filter.forEach(el => {
+                let a = `${el.column} ${el.type} $${filterIndex}`
+                filterIndex++
+                vals.push(el.value)
+                filterArr.push(a)
+            });
+            if(filterArr.length > 0){
+                sqlStatement += ` WHERE ${filterArr.join(" AND ")}`
+            }
+            if(limit){
+                sqlStatement += ` LIMIT ${limit}`
+            }
+            if(offset){
+                sqlStatement+= ` OFFSET ${offset}`
+            }
+            let cands = await this._db.query(sqlStatement, vals)
+            let result = []
+            cands.forEach((c)=>{
+                result.push(new ScheduleModel(c))
+            })
+            return result
+        }
 }
